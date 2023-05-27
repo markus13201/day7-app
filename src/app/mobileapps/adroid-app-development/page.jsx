713ca4ce947040cdafd2b58f8d8ec4fcefaa7fa4
MarkusKha <120775572+markus13201@users.noreply.github.com>
2023-05-27 21:50:50 +1000

    installed chakra-ui

---
+++ src/app/mobileapps/adroid-app-development/page.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+function Android() {
+  return (
+    <div>page</div>
+  )
+}
+
+export default Android
---
+++ src/app/mobileapps/ios-app-development/page.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+function Ios() {
+  return (
+    <div>page</div>
+  )
+}
+
+export default Ios
